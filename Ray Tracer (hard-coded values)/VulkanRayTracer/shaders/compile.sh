cf9ad918d97c0a0677428b28cbbf2801dc938b70
Felipe Caputo <felipecaputo90@gmail.com>
2018-11-19 14:46:28 -0200

    further improve ray tracer on compute shader

--- Ray Tracer (hard-coded values)/VulkanRayTracer/shaders/compile.sh
+++ Ray Tracer (hard-coded values)/VulkanRayTracer/shaders/compile.sh
@@ -1,3 +1,1 @@
-/Users/felipecaputo/Documents/DevelopmentTools/vulkansdk-macos-1.1.73.0/macOS/bin/glslangValidator -V shader.vert
-/Users/felipecaputo/Documents/DevelopmentTools/vulkansdk-macos-1.1.73.0/macOS/bin/glslangValidator -V shader.frag
 /Users/felipecaputo/Documents/DevelopmentTools/vulkansdk-macos-1.1.73.0/macOS/bin/glslangValidator -V shader.comp
